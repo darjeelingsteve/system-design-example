ab5dc9a942706d7bd801c19f5d94ad41b49b6112
Stephen Anthony <steve@darjeelingapps.com>
2019-11-07 10:24:51 +0000

    Show a text field and allow the font to be changed

--- System Design Example/ViewController.swift
+++ System Design Example/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
+/// A view controller showing how to modify fonts using
+/// `UIFontDescriptor.SystemDesign`.
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private var textView: UITextView!
+    
+    @IBAction private func changeFont(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            updateTextFieldFont(withDesign: .default)
+        case 1:
+            updateTextFieldFont(withDesign: .rounded)
+        case 2:
+            updateTextFieldFont(withDesign: .serif)
+        case 3:
+            updateTextFieldFont(withDesign: .monospaced)
+        default:
+            fatalError("Unnavailable design selected.")
+        }
     }
-
-
+    
+    private func updateTextFieldFont(withDesign design: UIFontDescriptor.SystemDesign) {
+        guard let descriptor = textView.font?.fontDescriptor.withDesign(design) else {
+            print("Could not update font.")
+            return
+        }
+        textView.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: UIFont(descriptor: descriptor, size: 0))
+    }
 }
-
